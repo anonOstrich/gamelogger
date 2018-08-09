94f8745ee82df92b4bcf20eeb5cd94de3ea84bd2
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-08-09 18:56:58 +0300

    Kirjautuneille käyttäjillä mahdollisuus muokata lisättyjen pelien tietoja. Linkki yksittäisen pelin esittelysivulla

--- application/games/views.py
+++ application/games/views.py
@@ -46,3 +46,32 @@ def games_view(game_id):
         return render_template("error.html", error = "Peliä ei ole olemassa")
     return render_template("/games/single.html", game = g)
     
+@app.route("/games/<game_id>/modify", methods=["GET", "POST"])
+def games_modify(game_id):
+    g = Game.query.get(game_id)
+    if not g: 
+        return render_template("error.html", error = "Peliä ei ole olemassa")
+    if request.method == "GET":
+        form = GameForm()
+        form.name.data = g.name
+        form.year.data = g.year
+        form.description.data = g.description
+        form.developer.data = g.developer
+        return render_template("/games/modify.html", form = form, id=game_id)
+    
+    form = GameForm(request.form)
+    if not form.validate():
+        render_template("games/modify.html", form = form, id = game_id)
+    g.name = form.name.data
+    g.year = form.year.data
+    g.description = form.description.data
+    g.developer = form.developer.data
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+    
+def games_delete():
+    return ":)" 
+
+
