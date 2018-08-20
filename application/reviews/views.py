3a855d32e565677470727694dc172c04ed2cd084
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-08-20 12:09:08 +0300

    Arvioiden muokkausmahdollisuus
    Arviotekstin lopussa nappi, josta pääsee muokkaamaan arvion sisältöä. Toistaiseksi pystyy tekemään kuka tahansa, eikä muokkaamisesta ilmoiteta arvion yhteytessä.

--- application/reviews/views.py
+++ application/reviews/views.py
@@ -33,4 +33,25 @@ def reviews_create(game_id):
 @login_required
 def reviews_show(game_id):
     return render_template("reviews/new.html", form = ReviewForm(), game = Game.query.get(game_id))
-    
+
+#TODO: vain admin (ja kirjoittaja jonkin aikaa lisäämisen jälkeen?) pystyy muokkaamaan
+@app.route("/<review_id>/modify", methods=["GET", "POST"])
+@login_required
+def reviews_modify(review_id):
+    r = Review.query.get(review_id)
+    if not r: 
+        return render_template("error.html", error = "Arvostelua ei löydy tietokannasta")
+    if request.method == "GET": 
+        form = ReviewForm()
+        form.text.data = r.text
+        form.points.data = r.points
+        return render_template("reviews/modify.html", form = form, game = Game.query.get(r.game_id), review_id = review_id)
+
+    form = ReviewForm(request.form)
+    if not form.validate(): 
+        return render_template("reviews/modify.html", form=form, game = Game.query.get(r.game_id), review_id = review_id)
+
+    r.text = form.text.data
+    r.points = form.points.data
+    db.session.commit()
+    return redirect(url_for("games_view", game_id = r.game_id))
