28f41e9e1315b9b95289767c00ba070d546a9e36
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-08-20 12:39:14 +0300

    Refaktoroitu arviolomakeluokkaa
    Tuotetaan vaihtoehdot omassa metodissaa. Opittiin käytännön vastoinkäymisistä, että Python on Javaa tarkempi siinä, että tiedostossa metodi on määriteltävä ennen sen ensimmäistä kutsumista.

--- application/reviews/forms.py
+++ application/reviews/forms.py
@@ -2,17 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, RadioField, validators
 
 class ReviewForm(FlaskForm):
+
+    def points_choices():
+        result=[]
+        for i in range(1, 11):
+            result.append((i, i))
+        return result    
+    
     text = TextAreaField("Arvostelu", [validators.Length(min=2, max=2000, message="Arvion oltava 2-2000 merkkiä pitkä")])
     points = RadioField("Arvosana", [validators.InputRequired(message="Valitse arvosana")], 
-                        choices=[(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8), (9,9), (10,10)], coerce=int)
+                        choices=points_choices(), coerce=int)
     
-    # Saisiko myöhemmin toimimaan?
-    #def points_choices():
-        #result=[]
-        #for i in range(1, 10):
-            #result.append((i, i))
-        #return result        
-        
     
     class Meta: 
         csrf = False
