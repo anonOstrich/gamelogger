76b3bd7b1734fc4abed0e1bdb479c3ff4a255f17
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-08-30 23:28:08 +0300

    Rekisteröitymisessä on vahvistettava salasana

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -9,8 +9,11 @@ class LoginForm(BaseForm):
 class RegisterForm(BaseForm): 
     name = StringField("Nimi", length_validators(max=100))
     username = StringField("Käyttäjätunnus", length_validators(max=100))
-    password = PasswordField("Salasana (vähintään 6 merkkiä)", length_validators(6, 32))
-    description = TextAreaField("Lyhyt kuvaus itsestäsi (vapaaehtoinen)", length_validators(max=1000))
+    password = PasswordField("Salasana (vähintään 6 merkkiä)",
+     length_validators(6, 32) + [validators.InputRequired(), 
+     validators.EqualTo("password_confirm", message = "Salasanat eivät täsmää")])
+    password_confirm = PasswordField("Toista salasana", length_validators(6, 32))
+    description = TextAreaField("Lyhyt kuvaus itsestäsi (vapaaehtoinen)", length_validators(max=1000, optional=True))
 
 class DescriptionForm(BaseForm):
     description = TextAreaField("", length_validators(max=1000))
